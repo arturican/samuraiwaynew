d712a741ae03f0c4069db81316bd0d614bec4a41
arturican <arturican@gmail.com>
2023-07-25 23:02:17 +0300

    14-16 урок

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import s from  './Header.module.css'
 const Header = () => (
-    <header className='header'>
-        <img src='https://www.logodesign.net/images/nature-logo.png' alt='Logo'/>
+    <header className={s.header}>
+        <img className={s.logo} src='https://www.logodesign.net/images/nature-logo.png' alt='Logo'/>
+        <ul className={s.ul}>
+            <li className={s.li}>
+                <a href='#'>Главная</a>
+            </li>
+            <li>
+                <a href='#'>О нас</a>
+            </li>
+            <li>
+                <a href='#'>Услуги</a>
+            </li>
+            <li>
+                <a href='#'>Контакты</a>
+            </li>
+        </ul>
     </header>
 );
 
