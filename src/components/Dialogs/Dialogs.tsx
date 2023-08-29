fe873e27c6bce9125e8199901efe9a42be1f3836
arturican <arturican@gmail.com>
2023-08-29 06:43:11 +0300

    Дописал компоненту Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+export type DialogItemPropsType = {
+    name: string
+    id: number
+}
+
+export type MessagePropsType = {
+    message: string
+}
+export const DialogItem = (props: DialogItemPropsType) => {
+    return (
+        <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+    )
+}
+
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div>
+            {props.message}
+        </div>
+    )
+}
 
 export const Dialogs = () => {
     return (
-        <div className={s.div}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <DialogItem id={1} name={"Artur"}/>
+                <DialogItem id={2} name={"Dilpopo"}/>
+            </div>
+            <div className={s.message}>
+                <Message message={'Hi'}/>
+                <Message message={'Lol kek'}/>
+            </div>
         </div>
+
     );
 };
 
