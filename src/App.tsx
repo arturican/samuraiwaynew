b05b8d83addbccd76fda0ebea73ab810e01c3f6b
arturican <arturican@gmail.com>
2023-11-21 10:01:51 +0300

    изменил App.css => App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import s from './App.module.css';
 import Header from "./components/Header/Header";
 import Navigation from "./components/Sidebar/Navigation";
 import MyPost from "./components/Profile/MyPost";
 
 const App = () => (
-    <div className='app-wrapper'>
+    <div className={s.appWrapper}>
         <Header />
         <Navigation/>
         <MyPost/>
